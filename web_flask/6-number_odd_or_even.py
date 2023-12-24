e9d5e79d878ba54f4d753bc28a5de670da50ccc4
Garnet Matola <matolagarnet.com>
2023-12-24 15:26:40 -0800

    syntex fix

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -43,7 +43,7 @@ def num_template(n):
     return render_template("5-number.html", n=n)
 
 
-@app.route("/number_odd_or_even/<int:n>", strict_slashes=False)
+@app.route("/number_odd_or_even/<int:n>")
 def number_odd_or_even(n):
     """ defines a function that display a HTML page """
     return render_template("6-number_odd_or_even.html", n=n)
